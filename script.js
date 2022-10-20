1a6a6bf8b2254c27efa1d12f22c7a59237507420
Cooper Hall <cooperhall111@gmail.com>
2022-10-20 14:21:18 -0400

    css updates, change of header names, added mouseover

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 const h2 = document.createElement("h2");
-h2.textContent = "Welcome to My First Website!";
+h2.textContent = "Tis a work in progress!";
 
 document.querySelector("body").appendChild(h2);
+
+
+document.getElementById("h2").onmouseover = function() {mouseOver()};
+document.getElementById("h2").onmouseout = function() {mouseOut()};
+
+function mouseOver() {
+  document.getElementById("h2").style.color = "red";
+}
+
+function mouseOut() {
+  document.getElementById("h2").style.color = "black";
+}
